414b5ee8eaf58d49e4f9bafeb2358cfa516b8e6e
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2014-07-12 00:57:10 +0200

    Add measure of constructing hash with pre-frozen keys

--- hash_construct.rb
+++ hash_construct.rb
@@ -15,11 +15,27 @@ class Mock
  end
 end
 
+class MockFrozen
+ attr_accessor :k, :v, :counter
+
+ def initialize
+   @@counter ||= 0
+   @k = "key#{@@counter}".freeze
+   @v = "val#{@@counter}".freeze
+   @@counter += 1
+ end
+end
+
 data = []
 1..10.times {|t| data << (Mock.new()) }
 
+data_frozen = []
+1..10.times {|t| data << (MockFrozen.new()) }
+
 Benchmark.bm(30) do |x|
+  x.report("reduction frozen keys") { iterations.times { data_frozen.reduce({}) {|m, d| m[d.k] =  d.v; m } } }
   x.report("reduction") { iterations.times { data.reduce({}) {|m, d| m[d.k] =  d.v; m } } }
+  x.report("reduction with freeze") { iterations.times { data.reduce({}) {|m, d| m[d.k.freeze] =  d.v; m } } }
   x.report("all at once array") { iterations.times { a = Array.new; data.each {|d| a << d.k << d.v}; Hash[*a]} }
   x.report("element by element") { iterations.times { h = Hash.new; data.each {|d| h[d.k] = h[d.v]} } }
   x.report("all at once subarray") { iterations.times { a = Array.new; data.each {|d| a << ([d.k, d.v])}; Hash[a]} }
