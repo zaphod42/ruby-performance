1050f4c62e98c942ef665dcaf45246e4567ceec2
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2014-07-14 14:47:39 +0200

    Add more split alternatives: regexp const, single quoted string

--- split_string.rb
+++ split_string.rb
@@ -5,6 +5,8 @@ FROZEN_SEP = "::".freeze
 long = (["word"] * 1000).join(SEP)
 short = (["word"] * 4).join(SEP)
 
+REGEX = Regexp.new('::').freeze
+
 iterations = 100_000
 Benchmark.bm(23) do |x|
   x.report("long/regex") { iterations.times { long.split(/::/) } }
@@ -12,7 +14,9 @@ Benchmark.bm(23) do |x|
   x.report("long/string-constant") { iterations.times { long.split(SEP) } }
   x.report("long/string-frozen") { iterations.times { long.split(FROZEN_SEP) } }
   x.report("short/regex") { iterations.times { short.split(/::/) } }
+  x.report("short/regex const") { iterations.times { short.split(REGEX) } }
   x.report("short/string") { iterations.times { short.split("::") } }
+  x.report("short/sqstring") { iterations.times { short.split('::') } }
   x.report("short/string-constant") { iterations.times { short.split(SEP) } }
   x.report("short/string-frozen") { iterations.times { short.split(FROZEN_SEP) } }
 end
